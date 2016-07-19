3496ab2fb59b3b1b3b889ea938067e3a1b1e72e7
Sheehan Olver <solver@mac.com>
2016-07-18 17:30:12 -0700

    added fpcauchy

--- src/RiemannHilbert.jl
+++ src/RiemannHilbert.jl
@@ -5,6 +5,58 @@ module RiemannHilbert
 
 export cauchymatrix
 
+
+# represent a + b*log(ε)
+immutable LogNumber
+    constant::Complex128
+    log::Complex128
+end
+
+
+function fpcauchy(f::Fun,z::Dual)
+    x = mobius(domain(f),z)
+    if isinf(mobius(domain(f),Inf))
+        error("Not implemented")
+    end
+    cfs = coefficients(f,Chebyshev)
+    if realpart(x) ≈ 1
+        c = (log(dualpart(x))-log(2))/(2π*im) * sum(cfs)
+        r = 0.0
+        for k=2:2:length(cfs)-1
+            r += 1/(k-1)
+            c += r*2/(π*im)*cfs[k+1]
+        end
+        r = 1.0
+        for k=1:2:length(cfs)-1
+            r += 1/(k-2)
+            c += (r+1/(2k))*2/(π*im)*cfs[k+1]
+        end
+        c
+    elseif realpart(x) ≈ -1
+        v = (log(-dualpart(x))-log(2))/(2π*im)
+        if !isempty(cfs)
+            c = -v*cfs[1]
+        end
+        r = 0.0
+        for k=2:2:length(cfs)-1
+            r += 1/(k-1)
+            c += -r*2/(π*im)*cfs[k+1]
+            c += -v*cfs[k+1]
+        end
+        r = 1.0
+        for k=1:2:length(cfs)-1
+            r += 1/(k-2)
+            c += (r+1/(2k))*2/(π*im)*cfs[k+1]
+            c += v*cfs[k+1]
+        end
+        c
+    else
+        error("Not implemented")
+    end
+end
+
+
+
 function cauchymatrix(s::Bool,space,pts::Vector)
     n=length(pts)
     C=Array(Complex128,n,n)
