70743398927b1d576d180aca4ca7c55e5b583b54
Sheehan Olver <solver@mac.com>
2018-11-29 21:16:59 +0000

    Work on KdV

--- examples/KdV.jl
+++ examples/KdV.jl
@@ -1,38 +1,126 @@
- using ApproxFun, Plots
+ using ApproxFun, SingularIntegralEquations, RiemannHilbert, Plots
 
 
+struct ReflectionCoefficient{VM, VP} <: Function
+    V₋::VM
+    V₊::VP
+end
 
-
-
-M = 50
-d₋, d₊ = -M .. 0, 0 ..M
-D = Derivative()
-
-V₋ = Fun(x -> sech(x), d₋)
-V₊ = Fun(x -> sech(x), d₊)
+function ReflectionCoefficient(V, a=-50.0, x₀=0.0, b=50.0)
+    d₋, d₊ = a .. x₀ , x₀ .. b
+    V₋,V₊ = Fun(V, d₋), Fun(V, d₊)
+    ReflectionCoefficient{typeof(V₋),typeof(V₊)}(V₋,V₊)
+end
 
 # ψ'' + (V(x) + k^2) ψ = 0
+function (R::ReflectionCoefficient)(k)
+    k == 0 && return -one(ComplexF64)
 
-k = 0.0001
+    a,x₀ = endpoints(domain(R.V₋))
+    b = rightendpoint(domain(R.V₊))
+    D = Derivative()
+    V₋,V₊ = R.V₋, R.V₊
+    ψ = [ivp(); D^2  + (V₋ + k^2)] \ [exp(im*k*a), im*k*(exp(im*k*a)), 0.0]
 
-ψ = [ivp(); D^2  + (V₋ + k^2)] \ [exp(im*k*(-M)), im*k*(exp(im*k*(-M))), 0.0]
+    F = qr([rdirichlet(space(V₊)); rneumann(); D^2  + (V₊ + k^2)])
+    ϕ₊ = F \ [exp(im*k*b), im*k*(exp(im*k*b)), 0.0]
+    ϕ₋ = F \ [exp(-im*k*b), -im*k*(exp(-im*k*b)), 0.0]
 
-plot(ψ)
+    a,b = [ϕ₊(x₀)   ϕ₋(x₀);
+           ϕ₊'(x₀)  ϕ₋'(x₀)] \ [ψ(x₀); ψ'(x₀)]
+    b/a
+end
+
+# use multiple threads since reflection coefficient is slow
+function tvalues(f, d, n)
+    p = points(d, n)
+    F = similar(p, ComplexF64)
+    Threads.@threads for k=1:length(p)
+        F[k] = R(p[k])
+    end
+    F
+end
+
+tFun(f, d::Space, n) = Fun(d, transform(d,tvalues(f,d,n)))
+
+tFun(f, d, n) = tFun(f, Space(d), n)
+
+R = ReflectionCoefficient(x -> 0.1sech(x))
+@time ρ = tFun(R, -5.0..5, 601)
+plot(abs.(ρ.coefficients); yscale=:log10)
+plot(ρ)
 
 
-ϕ₊ = [rdirichlet(); rneumann(); D^2  + (V₊ + k^2)] \ [exp(im*k*(M)), im*k*(exp(im*k*(M))), 0.0]
-ϕ₋ = [rdirichlet(); rneumann(); D^2  + (V₊ + k^2)] \ [exp(-im*k*(M)), -im*k*(exp(-im*k*(M))), 0.0]
-
-a,b = [ϕ₊(0)   ϕ₋(0);
-       ϕ₊'(0)  ϕ₋'(0)] \ [ψ(0); ψ'(0)]
+G = [1-abs2.(ρ) -conj.(ρ);
+     ρ           1.0]
 
 
-plot(ψ; legend=false)
-    plot!(a*ϕ₊ + b*ϕ₋)
+Φ = transpose(rhsolve(transpose(G), 2*4*100))
+
+Φ(0.1+eps()im)
 
 
-b/a
 
+
+Φ(100.0im)
+
+Φ(0.1-eps()im)*G(0.1)
+
+domain(ρ)
+
+v = tvalues(R, Chebyshev(-10..10), 100)
+
+
+R(8.0)
+
+plot(real.(v))
+    plot!(imag.(v))
+
+R(0.0)
+R(-0.0000)
+
+
+
+ρ(0.1)
+R(0.1)
+
+plot(ρ)
+
+ρ.coefficients
+
+ρ.coefficients
+
+
+f = R
+n = 100
+d = Chebyshev(-10..10)
+
+
+
+@time R(1.0)
+ret = Vector{ComplexF64}(undef,100)
+@time Threads.@threads for k=1:length(ret)
+    ret[k] = R(k/10)
+end
+
+@time for k=1:100
+    ret[k] = R(k/10)
+end
+
+@time pmap(R, 1.0:4)
+
+
+ρ = Fun(R, -10..10, 200)
+
+@time R(10.0)
+
+ks = -10.0:0.1:10.0
+Rks = R.(ks)
+
+plot(ks, real.(Rks))
+    plot!(ks, imag.(Rks))
+
+R(0.1+0.1im)
 
 
 #####
