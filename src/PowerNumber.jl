95421112bc34a52713021c5a213558de91039732
Sheehan Olver <solver@mac.com>
2019-08-08 16:25:38 +0100

    try getting stieltjhes moment working

--- src/PowerNumber.jl
+++ src/PowerNumber.jl
@@ -24,7 +24,16 @@ Base.isapprox(a::PowerNumber, b::PowerNumber; opts...) = ≈(powerpart(a), power
 
 for f in (:+, :-)
     @eval begin
-        $f(a::PowerNumber, b::PowerNumber) = if a.α == b.α PowerNumber($f(a.s, b.s), $f(a.c, b.c), a.α) else error("Exponents must be equal.") end
+        function $f(a::PowerNumber, b::PowerNumber) 
+            if a.α == b.α PowerNumber($f(a.s, b.s), $f(a.c, b.c), a.α) 
+            elseif a.α < b.α && b.α ≥ 0
+                PowerNumber(a.s, $f(a.c, b.c), a.α) 
+            elseif b.α < a.α && a.α ≥ 0
+                PowerNumber($f(b.s), $f(a.c, b.c), b.α) 
+            else
+                error("Not implemented")
+            end
+        end
         $f(l::PowerNumber, b::Number) = PowerNumber(l.s, $f(l.c, b), l.α)
         $f(a::Number, l::PowerNumber) = PowerNumber($f(l.s), $f(a, l.c), l.α)
     end
@@ -39,12 +48,52 @@ for Typ in (:Bool, :Number)
     end
 end
 
+*(a::RiemannDual, b::PowerNumber) = realpart(a)*b
+*(a::PowerNumber, b::RiemannDual) = a*realpart(b)
+
 /(l::PowerNumber, b::Number) = PowerNumber(l.s/b, l.c/b, l.α)
+/(l::PowerNumber, b::PowerNumber) = l*inv(b)
+/(l::Number, b::PowerNumber) = l*inv(b)
 
 exp(l::PowerNumber)::ComplexF64 = exp(l.c)
 
 for op in (:real, :imag, :conj)
     @eval $op(l::PowerNumber) = PowerNumber($op(powerpart(l)), $op(finitepart(l)), exppart(l))
+end
+
+function sqrt(x::RiemannDual{T}) where T 
+    if realpart(x) == 0 
+        PowerNumber(sqrt(epsilon(x)),zero(T),one(T)/2) 
+    elseif isinf(realpart(x))
+        PowerNumber(sqrt(epsilon(x)),zero(T),-one(T)/2) 
+    else
+        RiemannDual(sqrt(dual(x)))
+    end
+end
+^(x::RiemannDual{T}, α::Number) where T = realpart(x) == 0 ? PowerNumber(epsilon(x)^α,zero(T),α) : RiemannDual(dual(x)^α)
+
+function inv(x::PowerNumber) 
+    a,b, α = powerpart(x),finitepart(x),exppart(x)
+    if b == 0 || α < 0 
+        PowerNumber(inv(a), zero(b), -α) 
+    else # α > 0
+        PowerNumber(-b/a^2, inv(b), α)
+    end
+end
+
+function *(x::PowerNumber, y::PowerNumber)
+    a,b, α = powerpart(x),finitepart(x),exppart(x)
+    c,d, γ = powerpart(y),finitepart(y),exppart(y)
+    α ≤ γ || return y*x
+    if α == 0.0
+        (a+b) * y
+    elseif α ≥ α+γ > 0
+        PowerNumber(a*c,b*d, α+γ)
+    elseif α+γ == 0
+        PowerNumber(a*d,a*c+b*d, α)
+    else
+        error("Not implemented for $α, $γ")
+    end
 end
 
 Base.show(io::IO, x::PowerNumber) = print(io, "($(powerpart(x)))ε^$(exppart(x)) + ($(finitepart(x)))")
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 using ApproxFun, SingularIntegralEquations, DualNumbers, RiemannHilbert, LinearAlgebra, FastTransforms, SpecialFunctions, Test
 import ApproxFunBase: ArraySpace, pieces, dotu, interlace
-import RiemannHilbert: RiemannDual, LogNumber, PowerNumber, ProductCondition, fpstieltjesmatrix!, fpstieltjesmatrix, orientedleftendpoint, orientedrightendpoint, finitepart, fpcauchymatrix, collocationvalues, collocationpoints
+import RiemannHilbert: RiemannDual, LogNumber, PowerNumber, productcondition, fpstieltjesmatrix!, fpstieltjesmatrix, orientedleftendpoint, orientedrightendpoint, finitepart, fpcauchymatrix, collocationvalues, collocationpoints
 import SingularIntegralEquations: stieltjesmoment, stieltjesmoment!, undirected, Directed, ⁺, ⁻, istieltjes
 import SingularIntegralEquations.HypergeometricFunctions: speciallog
 
@@ -39,7 +39,10 @@ import SingularIntegralEquations.HypergeometricFunctions: speciallog
     for z in (RiemannDual(-1,-1), RiemannDual(-1,exp(0.1im)), RiemannDual(-1,exp(-0.1im)))
         @test stieltjesjacobimoment(0.5,0,0,z)(h) ≈ stieltjesjacobimoment(0.5,0,0,realpart(z)+epsilon(z)h) atol=1E-4
     end
-end
+
+    z = RiemannDual(1,2)
+    @test 1/(1-z(h)) ≈ (1/(1-z))(h) rtol=0.0001
+end 
 
 @testset "Legendre Cauchy" begin
     f = Fun(exp,Legendre())
@@ -82,11 +85,44 @@ end
 end
 
 @testset "PowerNumbers arithmetic" begin
+    h = 0.000000001
+    @test inv(PowerNumber(1.0,2.0,-1/2))(h) ≈ inv(h^(-1/2) + 2) rtol = 0.001
+    @test inv(PowerNumber(1.0,0.0,1/2))(h) ≈ inv(h^(1/2)) rtol = 0.001
+    @test inv(PowerNumber(1.0,2.0,1/2))(h) ≈ inv(h^(1/2)+2) rtol = 0.001
+
+
+    @test sqrt(RiemannDual(0.0,2.0)) == PowerNumber(sqrt(2),0.0,1/2) 
+    @test sqrt(RiemannDual(0.0,2.0))(h) == sqrt(2*h)
+    @test RiemannDual(0.0,2.0)^(1/3) == PowerNumber(2^(1/3),0.0,1/3) 
+    @test (RiemannDual(0.0,2.0)^(1/3))(h) == (2*h)^(1/3)
+
     @test real(PowerNumber(2im,im+1,0.5)) == PowerNumber(0,1,0.5)
     @test imag(PowerNumber(2im,im+1,0.5)) == PowerNumber(2,1,0.5)
     @test conj(PowerNumber(2im,im+1,0.5)) == PowerNumber(-2im,1-im,0.5)
     
     @test (3*PowerNumber(0.01+3im,0.2im+1,0.4) - 4*PowerNumber(0.6-1.5im,1.5-0.3im,0.4))/3 == PowerNumber(-0.79+5.0im,-1.0 + 0.6im,0.4)
+
+    @test SingularIntegralEquations.sqrtx2(RiemannDual(1.0,2.0)) ≈ PowerNumber(2.0,0.0,0.5)
+
+    x = Fun()
+    f = 1/sqrt(1-x^2)
+    @test stieltjes(f,RiemannDual(1.0,2.0))(h) ≈ stieltjes(f,1+2h) rtol = 0.001
+    f = exp(x)/sqrt(1-x^2)
+    @test stieltjes(f,RiemannDual(1.0,2.0))(h) ≈ stieltjes(f,1+2h) rtol = 0.001
+
+    f = sqrt(1-x^2)
+    @test stieltjes(f,RiemannDual(1.0,2.0))(h) ≈ stieltjes(f,1+2h) rtol = 0.001
+    f = exp(x)*sqrt(1-x^2)
+    @test stieltjes(f,RiemannDual(1.0,2.0))(h) ≈ stieltjes(f,1+2h) rtol = 0.001
+
+    f = Fun(JacobiWeight(0.0,0.5,Jacobi(0.0,0.5)), [1.0])
+    stieltjes(f, RiemannDual(1.0,2.0))
+    n = 0
+    β,α = 0.0,0.5; a,b,c = n+1,n+α+1,2n+α+β+2
+    z = RiemannDual(1.0,2.0); z= 2/(1-z);
+    
+    z
+    undirected(-z)^a*_₂F₁(a,b,c,z)
 end
 
 @testset "Interval FPStieltjes" begin
